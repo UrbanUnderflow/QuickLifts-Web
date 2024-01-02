e17ca02861e3a96eacc2c2b01ba391dd5186b09b
tdg10e <tremaine.grant@gmail.com>
2024-01-01 22:36:02 -0500

    website look and feel update
    updated the website look and feel

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,30 +1,42 @@
 import React from 'react';
 const Home = () => (
-  <div className="h-screen overflow-hidden relative flex flex-col" style={{backgroundColor: '#192126'}}>
-    <div className="relative z-20 h-screen flex items-center justify-center bg-black bg-opacity-50">
-      <div className="bg-white bg-opacity-50 p-10 rounded shadow-lg w-1/3 space-y-6 text-left ml-20" style={{color: '#E0FE10'}}>
-        <h1 className="text-4xl font-bold">QuickLifts - Your Personal Gym Guide</h1>
-        <p className="text-lg">
-          Welcome to QuickLifts, the ultimate guide for your gym workouts. Powered by AI, our app provides personalized workout plans to help you reach your fitness goals in 40 minutes. Start your fitness journey with us and make every minute count.
-          <br />
-          Available now on the App Store and Play Store!
-        </p>
-        <a href="mailto:quickliftsapp@gmail.com">
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold my-8 py-2 px-4 rounded">Contact us</button>
-        </a>
+  <div className="h-screen overflow-hidden relative flex flex-col"
+       style={{backgroundColor: '#141A1E'}}>
+    <div className="pt-10 px-6 sm:px-24 flex justify-between">
+      <div className="flex items-center text-white font-bold">
+        <img src="/pulse-logo.png" alt="Pulse Logo" className="h-12" />
+      </div>
+      <div className="text-white font-bold">
+        <a href="mailto:pulsefitnessapp@gmail.com" className="text-white">Contact Us</a>
       </div>
     </div>
+
+    <div className="flex flex-col sm:flex-row p-6 sm:p-20">
+      <div className="sm:pl-48 pt-10 font-bold text-white text-4xl sm:text-5xl">
+        Beat Your Best,<br />Share Your Victoy<br />
+        <div className="mt-10">
+          <button className="px-8 py-2 h-12 text-sm bg-blue-600 text-white font-bold rounded">
+            Download Now
+          </button>
+        </div>
+      </div>
+      <div className="mt-10 sm:mt-0">
+        <img src="/pulse-phone-mobile.png" alt="Phone Mobile" className="w-full md:hidden h-auto" />
+        <img src="/pulse-phone.png" alt="Phone" className="hidden md:block w-full md:w-[1048px] h-auto" />
+      </div>
+    </div>
+
     <footer className="absolute bottom-0 z-20 w-full h-20 bg-black bg-opacity-50 flex items-center justify-center">
       <div className="text-white text-center space-y-2">
-        <p>© {new Date().getFullYear()} QuickLifts. All rights reserved.</p>
+        <p>© {new Date().getFullYear()} Pulse Fitness. All rights reserved.</p>
         <div className="flex justify-center space-x-4">
           <a href="/terms" className="text-blue-400 hover:text-blue-600">Terms of Use</a>
-          <a href="/privacyPolicy" className="text-blue-400 hover:text-blue-600">Privacy Policy</a>
+          <a href="/privacy" className="text-blue-400 hover:text-blue-600">Privacy Policy</a>
         </div>
       </div>
     </footer>
   </div>
 );
 
-
 export default Home;
+
