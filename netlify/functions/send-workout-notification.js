610fe048be3f3d82e200b7c6d408b87091597e98
tdg10e <tremaine.grant@gmail.com>
2024-04-10 18:34:13 -0400

    Update send-workout-notification.js
    new key

--- netlify/functions/send-workout-notification.js
+++ netlify/functions/send-workout-notification.js
@@ -24,7 +24,7 @@ if (admin.apps.length === 0) {
 const auth = new GoogleAuth({
   credentials: {
     client_email: 'firebase-adminsdk-1qxb0@quicklifts-dd3f1.iam.gserviceaccount.com',
-    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    private_key: process.env.FIREBASE_SECRET_KEY.replace(/\\n/g, '\n'),
   },
   scopes: ['https://www.googleapis.com/auth/cloud-platform'],
 });
