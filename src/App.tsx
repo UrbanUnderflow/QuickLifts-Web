2d7ec8cb5e637c30bc69e8beb00c4355217660a6
tdg10e <tremaine.grant@gmail.com>
2024-11-17 14:27:21 -0500

    serverside
    update index serverside

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { HelmetProvider } from 'react-helmet-async';
-import BaseLayout from './components/BaseLayout';
 import Home from './pages/Home';
 import PrivacyPolicy from './pages/PrivacyPolicy';
 import Terms from './pages/Terms';
@@ -13,23 +11,19 @@ import PublicProfileView from './pages/PublicProfileView';
 
 const App: React.FC = () => {
   return (
-    <HelmetProvider>
-      <Router>
-        <BaseLayout>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/privacyPolicy" element={<PrivacyPolicy />} />
-            <Route path="/terms" element={<Terms />} />
-            <Route path="/completeOnboarding" element={<CompletedOnboarding />} />
-            <Route path="*" element={<div>404 Not Found</div>} />
-            <Route path="/workoutPreview" element={<WorkoutPreviewer />} />
-            <Route path="/collection" element={<CollectionView />} />
-            <Route path="/starterpack" element={<Checklist />} />
-            <Route path="/:username" element={<PublicProfileView />} />
-          </Routes>
-        </BaseLayout>
-      </Router>
-    </HelmetProvider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/privacyPolicy" element={<PrivacyPolicy />} />
+        <Route path="/terms" element={<Terms />} />
+        <Route path="/completeOnboarding" element={<CompletedOnboarding />} />
+        <Route path="*" element={<div>404 Not Found</div>} />
+        <Route path="/workoutPreview" element={<WorkoutPreviewer />} />
+        <Route path="/collection" element={<CollectionView />} />
+        <Route path="/starterpack" element={<Checklist />} />
+        <Route path="/:username" element={<PublicProfileView />} />
+      </Routes>
+    </Router>
   );
 }
 
