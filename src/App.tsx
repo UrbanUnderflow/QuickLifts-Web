3b05407bf5db8dc98e128d15ceba8c2c3442878d
tdg10e <tremaine.grant@gmail.com>
2024-11-17 13:54:55 -0500

    HelmerProvider
    Added HelmetProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { HelmetProvider } from 'react-helmet-async';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import PrivacyPolicy from './pages/PrivacyPolicy';
@@ -11,20 +12,23 @@ import PublicProfileView from './pages/PublicProfileView';
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/privacyPolicy" element={<PrivacyPolicy />} />
-        <Route path="/terms" element={<Terms />} />
-        <Route path="/completeOnboarding" element={<CompletedOnboarding />} />
-        <Route path="*" element={<div>404 Not Found</div>} />
-        <Route path="/workoutPreview" element={< WorkoutPreviewer />} />
-        <Route path="/collection" element={< CollectionView />} />
-        <Route path="/starterpack" element={< Checklist />} />
-        <Route path="/:username" element={<PublicProfileView />} />
+    <HelmetProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/privacyPolicy" element={<PrivacyPolicy />} />
+          <Route path="/terms" element={<Terms />} />
+          <Route path="/completeOnboarding" element={<CompletedOnboarding />} />
+          <Route path="*" element={<div>404 Not Found</div>} />
+          <Route path="/workoutPreview" element={< WorkoutPreviewer />} />
+          <Route path="/collection" element={< CollectionView />} />
+          <Route path="/starterpack" element={< Checklist />} />
+          <Route path="/:username" element={<PublicProfileView />} />
 
-      </Routes>
-    </Router>
+        </Routes>
+      </Router>
+    </HelmetProvider>
+
   );
 }
 
