7a8bead64da4feedd98bbe2ad386dd25b3114d37
tdg10e <tremaine.grant@gmail.com>
2024-11-18 11:17:20 -0500

    prefixer
    added prefixer

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -9,11 +9,6 @@ self.__BUILD_MANIFEST = {
   "lowPriorityFiles": [],
   "rootMainFiles": [],
   "pages": {
-    "/": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/index.js"
-    ],
     "/[username]": [
       "static/chunks/webpack.js",
       "static/chunks/main.js",
