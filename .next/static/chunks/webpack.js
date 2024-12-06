f6d4bb0827503447d53fc9c46381dc9c43a9e20d
tdg10e <tremaine.grant@gmail.com>
2024-12-06 10:40:09 -0500

    back arrow
    Added back arrow

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "531e040fcab1f754"; }
+/******/ 		__webpack_require__.h = function() { return "4514d21b221fe26e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
