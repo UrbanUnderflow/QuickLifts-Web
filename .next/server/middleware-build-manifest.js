9d90e45e052ec9775edbdf2c0bf702e49607b3cc
tdg10e <tremaine.grant@gmail.com>
2024-12-24 12:26:25 -0500

    header icon
    updated header icon

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -19,10 +19,10 @@ self.__BUILD_MANIFEST = {
       "static/chunks/main.js",
       "static/chunks/pages/_error.js"
     ],
-    "/connect": [
+    "/dashboard": [
       "static/chunks/webpack.js",
       "static/chunks/main.js",
-      "static/chunks/pages/connect.js"
+      "static/chunks/pages/dashboard.js"
     ],
     "/rounds": [
       "static/chunks/webpack.js",
