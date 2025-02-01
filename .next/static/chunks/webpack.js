3615ee3c6d3ffa824f431bb629805bf79f4a441e
tdg10e <51893324+tdg10e@users.noreply.github.com>
2025-01-31 21:40:20 -0500

    pp
    Remove privacy policy

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d76f622d71e19507"; }
+/******/ 		__webpack_require__.h = function() { return "88fdcaf42428f81f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
