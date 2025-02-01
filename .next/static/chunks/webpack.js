9c382d8c589a86b0a891a8ebbc83b9d865e61b8c
tdg10e <51893324+tdg10e@users.noreply.github.com>
2025-01-31 21:17:46 -0500

    removed terms page
    removed terms page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b9c7e7446acad6d8"; }
+/******/ 		__webpack_require__.h = function() { return "b23b2435ccbd1aa4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
