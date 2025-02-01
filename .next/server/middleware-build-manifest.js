0dbfad6b413eccfa9a4798d12a825b2cace82457
tdg10e <51893324+tdg10e@users.noreply.github.com>
2025-01-31 20:28:53 -0500

    fixing terms
    fixing terms

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -9,6 +9,11 @@ self.__BUILD_MANIFEST = {
   "lowPriorityFiles": [],
   "rootMainFiles": [],
   "pages": {
+    "/": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/index.js"
+    ],
     "/_app": [
       "static/chunks/webpack.js",
       "static/chunks/main.js",
@@ -23,6 +28,11 @@ self.__BUILD_MANIFEST = {
       "static/chunks/webpack.js",
       "static/chunks/main.js",
       "static/chunks/pages/stacks.js"
+    ],
+    "/starter-pack": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/starter-pack.js"
     ],
     "/terms": [
       "static/chunks/webpack.js",
