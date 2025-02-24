b9ba8b276e0145b175e6e5c53eca0be4c60b0637
tdg10e <51893324+tdg10e@users.noreply.github.com>
2025-02-24 09:51:09 -0500

    Update get-challenge-by-id.js
    reduced redundancy

--- netlify/functions/get-challenge-by-id.js
+++ netlify/functions/get-challenge-by-id.js
@@ -1,9 +1,6 @@
 const admin = require('firebase-admin');
 
 // Add guard clauses to check for required environment variables
-if (!process.env.FIREBASE_SECRET_KEY) {
-  throw new Error('Missing FIREBASE_SECRET_KEY environment variable.');
-}
 if (!process.env.FIREBASE_PRIVATE_KEY) {
   throw new Error('Missing FIREBASE_PRIVATE_KEY environment variable.');
 }
@@ -14,19 +11,14 @@ const headers = {
   'Access-Control-Allow-Methods': 'GET, POST, OPTIONS'
 };
 
+// Initialize Firebase only once
 if (admin.apps.length === 0) {
   admin.initializeApp({
     credential: admin.credential.cert({
-      "type": "service_account",
-      "project_id": "quicklifts-dd3f1",
-      "private_key_id": process.env.FIREBASE_PRIVATE_KEY,
-      "private_key": process.env.FIREBASE_SECRET_KEY.replace(/\\n/g, '\n'),
-      "client_email": "firebase-adminsdk-1qxb0@quicklifts-dd3f1.iam.gserviceaccount.com",
-      "client_id": "111494077667496751062",
-      "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-      "token_uri": "https://oauth2.googleapis.com/token",
-      "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-      "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-1qxb0%40quicklifts-dd3f1.iam.gserviceaccount.com"
+      type: "service_account",
+      project_id: "quicklifts-dd3f1",
+      private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      client_email: "firebase-adminsdk-1qxb0@quicklifts-dd3f1.iam.gserviceaccount.com"
     })
   });
 }
